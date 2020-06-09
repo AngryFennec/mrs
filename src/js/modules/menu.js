b4c401d461c4c4747ec02a940434733e4c77acd1
AngryFennec <shellfix@gmail.com>
2020-06-09 14:43:45 +0300

    Мобильное меню

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -7,14 +7,13 @@
 // });
 
 $(".header__burger").on('click', function(){
-  $('.header nav, .header__top').fadeToggle(400);
+  $('.header nav, .header__price, .header__top').fadeToggle(400).css("display","block");
   $('.header__burger img').toggleClass('none');
   $('.page-body').toggleClass('page-body--overflow');
 
   $('.header nav, .header__top').on('click', function(){
     if($('.header').width() <=1024) {
-      $('.header nav, .header__top').hide();
-      $('.header__price').hide();
+      $('.header nav, .header__price, .header__top').hide();
       $('.header__burger img').eq(0).removeClass('none');
       $('.header__burger img').eq(1).addClass('none');
       $('.page-body').removeClass('page-body--overflow');
@@ -24,8 +23,7 @@ $(".header__burger").on('click', function(){
 
 $(window).on('resize', function(){
   if($('.header').width() > 1024) {
-    $('.header nav, .header__top').show();
-    $('.header__price').show();
+    $('.header nav, .header__price, .header__top').show();
     $('.header__burger img').eq(0).removeClass('none');
     $('.header__burger img').eq(1).addClass('none');
     $('.page-body').removeClass('page-body--overflow');
